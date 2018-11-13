8071717f6e3b7519961f76f3f75db15d0ac01851
Enzojz <enzojz@gmail.com>
2018-11-13 21:55:37 +0100

    1.1

--- strings.lua
+++ strings.lua
@@ -8,6 +8,7 @@ Usage:
 Place a Station Name Sign around a station, by default there is a snapping track to help you align the sign onto the platform, use offset and distance options to place it accurately, choose the font and style before finish it.
 Use the Platform Signs finalizer when everything is done, the finalizer will inject the station name onto the sign. The name injected is the closest staion to the sign.
 If you want to place the station name sign on a combined sign, please uncheck the pole option.
+Renaming the sign and start with #, that will force the finalizer to inject any text you want on the sign
 
 2. Combined Sign
 A combined sign permets you have multiple signs on a same pole, you can have one or some rows of signs and in each row you have can zero to two signs.
@@ -25,7 +26,8 @@ Use the finalizer to inject the designated text on the sign.
 You can hide off the poles to make signs combined on other poles.
 
 3. Remover
-The remove helps you removing finished signs, since the models will be broken into many pieces once finished. Place a new sign around the area to clean, and use the remover to remove finished signs.
+The remove helps you removing all signs, since the models will be broken into many pieces once finished. 
+Place a remover around the area to clean, and reclick on the remover icon to remove signs around it.
 
 The font support comes with Livetext mod, there are two fonts supplied with Livetext mod: Lato (game build-in font) and Alte DIN 1451 Mittelschrift font which is free to use.
 
@@ -38,6 +40,13 @@ The Livetext mod use unicode, the following charset are supported:
 Attention: not all glyphs are present in DIN 1451 font
 
 To extend the charset to other languages or other fonts, for example CJK, a generator will be supplied later for Livetext mod. 
+
+Changelog
+1.1
+* Fixed crashes linked to the params failure
+* Improved the materials
+* Improved the usage of remover
+* Corrected the station name capture when the station has any subfix or have been renamed
 ]]
 
 function data()
